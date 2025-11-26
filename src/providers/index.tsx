64e7450cf8e600c031ae4e50044746c2109bfa86
Caleb anderson <buyananderson@gmail.com>
2025-11-26 18:48:21 -0500

    feat: update AppProviders to enable dark mode and restructure theme context

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -5,21 +5,27 @@ import { darkTheme, lightTheme } from './theme';
 import { queryClient } from '@/config/reactQueryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { AuthProvider } from '@/context/AuthContext';
+import { CustomThemeProvider } from '@/context/ThemeContext';
 
 export default function AppProviders({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const [darkMode] = useState(false);
+	const [darkMode] = useState(true);
 	const theme = useMemo(() => (darkMode ? darkTheme : lightTheme), [darkMode]);
 	return (
 		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
 			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				{children}
+				<CustomThemeProvider>
+					<CssBaseline />
+					{children}
+				</CustomThemeProvider>
 			</ThemeProvider>
 			<ReactQueryDevtools initialIsOpen={false} />
+			</AuthProvider>
 		</QueryClientProvider>
 	);
 }
