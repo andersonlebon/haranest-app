aa049a6d186402481f1c3d2edf8268abf0fff693
Caleb anderson <buyananderson@gmail.com>
2025-12-04 04:56:12 -0500

    feat: update ESLint configuration to disable specific TypeScript, React, and Next.js rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,23 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // ✅ TypeScript
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+
+      // ✅ React
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off",
+
+      // ✅ Next.js
+      "@next/next/no-img-element": "off",
+    },
+  },
+
 ];
 
 export default eslintConfig;
